0f205e125d681f6a7857d5455e613809788ff661
조용일 <yedn1128@naver.com>
2024-04-26 16:58:48 +0900

    전략 패턴 - 예제 1 ( 전략 패턴 예제 테스트 코드. 상속이 아니라 위임(interface)으로 )

--- advanced3/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ advanced3/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,5 +1,10 @@
 package hello.advanced.trace.strategy;
 
+import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.template.code.SubClassLogic1;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -34,5 +39,16 @@ public class ContextV1Test {
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - starTime;
         log.info("resultTime={}", resultTime);
+    }
+
+    @Test
+    void strategyV1() {
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        contextV1.execute();
+
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        contextV2.execute();
     }
 }
