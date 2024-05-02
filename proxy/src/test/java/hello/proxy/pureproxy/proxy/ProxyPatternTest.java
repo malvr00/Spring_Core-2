21e9a8b4ec245d389414f0ca8434914af8ba9453
조용일 <yedn1128@naver.com>
2024-05-02 16:07:32 +0900

    프록시 페턴 - 예제 코드 2 ( 프록시 패턴 적용 )

--- proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package hello.proxy.pureproxy.proxy;
 
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
 import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import hello.proxy.pureproxy.proxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,17 @@ public class ProxyPatternTest {
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
         // 3초 소모
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+        // 1초 소모
         client.execute();
         client.execute();
         client.execute();
