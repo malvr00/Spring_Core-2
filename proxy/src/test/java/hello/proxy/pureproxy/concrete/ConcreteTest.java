cb6c0ff692446e4aba83a8496ff4779c313fde3a
조용일 <yedn1128@naver.com>
2024-05-02 18:52:55 +0900

    구체 클래스 기반 프록시 - 예제 2 ( 구체 클래스를 상속 받아 데코레이터 패턴 적용 )

--- proxy/src/test/java/hello/proxy/pureproxy/concrete/ConcreteTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/concrete/ConcreteTest.java
@@ -2,6 +2,7 @@ package hello.proxy.pureproxy.concrete;
 
 import hello.proxy.pureproxy.concrete.code.ConcreteClient;
 import hello.proxy.pureproxy.concrete.code.ConcreteLogic;
+import hello.proxy.pureproxy.concrete.code.TimeProxy;
 import org.junit.jupiter.api.Test;
 
 public class ConcreteTest {
@@ -10,6 +11,14 @@ public class ConcreteTest {
     void noProxy() {
         ConcreteLogic concreteLogic = new ConcreteLogic();
         ConcreteClient concreteClient = new ConcreteClient(concreteLogic);
+        concreteClient.execute();
+    }
+
+    @Test
+    void addProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient concreteClient = new ConcreteClient(timeProxy);
         concreteClient.execute();
     }
 }
