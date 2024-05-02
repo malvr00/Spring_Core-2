8c33caeba8d0241faa748c78d3d6fc46d1407218
조용일 <yedn1128@naver.com>
2024-05-02 15:54:24 +0900

    프록시 패턴 - 예제 코드 2 ( 프록시 패턴 적용 전 )

---
+++ proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,18 @@
+package hello.proxy.pureproxy.proxy;
+
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import org.junit.jupiter.api.Test;
+
+public class ProxyPatternTest {
+
+    @Test
+    void noProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        // 3초 소모
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+}
