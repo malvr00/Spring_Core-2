cf9668c0bbacaf5fd2a4516a22accc154a16b024
조용일 <yedn1128@naver.com>
2024-07-04 13:10:35 +0900

    동적 프록시 기술 init

---
+++ dynamic/src/test/java/hello/proxy/pureproxy/concrete/ConcreteTest.java
@@ -0,0 +1,24 @@
+package hello.proxy.pureproxy.concrete;
+
+import hello.proxy.pureproxy.concrete.code.ConcreteClient;
+import hello.proxy.pureproxy.concrete.code.ConcreteLogic;
+import hello.proxy.pureproxy.concrete.code.TimeProxy;
+import org.junit.jupiter.api.Test;
+
+public class ConcreteTest {
+
+    @Test
+    void noProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        ConcreteClient concreteClient = new ConcreteClient(concreteLogic);
+        concreteClient.execute();
+    }
+
+    @Test
+    void addProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient concreteClient = new ConcreteClient(timeProxy);
+        concreteClient.execute();
+    }
+}
---
+++ dynamic/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,30 @@
+package hello.proxy.pureproxy.proxy;
+
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import org.junit.jupiter.api.Test;
+
+public class ProxyPatternTest {
+
+    @Test
+    void noProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        // 3초 소모
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+        // 1초 소모
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+}
